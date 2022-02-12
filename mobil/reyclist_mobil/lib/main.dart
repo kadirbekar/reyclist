5c6655f57cb80c24a4b399c1a4dbe2bc7cf51f18
aliaydogdu000 <aydogduali000@gmail.com>
2022-02-13 01:33:42 +0300

    qr and map page edits(2)

--- mobil/reyclist_mobil/lib/main.dart
+++ mobil/reyclist_mobil/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:reyclist_mobil/ui/QR/qr.dart';
 import 'package:reyclist_mobil/ui/home/home.dart';
+import 'package:reyclist_mobil/ui/map/map.dart';
 
 void main() => runApp(const Reyclist());
 
@@ -11,7 +13,7 @@ class Reyclist extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Reyclist',
-      home: HomePage(),
+      home: mapPage(),
     );
   }
 }
