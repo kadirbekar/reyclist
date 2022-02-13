2c8605c1ba41ebf8a424bfa92cdf4cb8a72af4c2
Kadir Bekar <kadir.bekr@gmail.com>
2022-02-13 03:42:28 +0300

    update user method added.

--- back_end/routers/user_router.js
+++ back_end/routers/user_router.js
@@ -2,5 +2,6 @@ const userRouter = require('express').Router()
 const userController = require('../controllers/user_controller')
 
 userRouter.post('/updateUserPoint', userController.updateUserPoint)
+userRouter.post('/updateUser/:id', userController.updateUser)
 
 module.exports = userRouter
